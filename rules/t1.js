66fc8e95e7b4e5225f8e60058cecbf3d0c088786
jax <josephaxisa@gmail.com>
2018-10-18 14:38:17 +0100

    rule T1

---
+++ rules/t1.js
@@ -0,0 +1,41 @@
+const isExempt = require('../lib/is-exempt');
+
+module.exports = function(
+	project,
+) {
+	let messages = [];
+	let rule = 'T1';
+	let ok = true;
+	let files = project.files || [];
+
+	for (let file of files) {
+		let views = file.views || [];
+		for (let view of views) {
+			let location = 'view: ' + view._view;
+			let path = '/projects/'+project.name+'/files/'+file._file_path+'#view:'+view._view;
+			let exempt = isExempt(file, rule) || isExempt(view, rule);
+			if (!view.derived_table) {
+				continue;
+			}
+			if (!(view.derived_table.hasOwnProperty('datagroup_trigger'))
+				&& (view.derived_table.hasOwnProperty('sql_trigger_value') || view.derived_table.hasOwnProperty('persist_for'))) {
+				ok = false;
+				messages.push({
+					location, path, rule, exempt, level: 'error',
+					description: `Triggered PDTs should use datagroups`,
+				});
+			}
+		}
+	}
+
+	if (ok) {
+		messages.push({
+			rule, level: 'info',
+			description: `PDT is using datagroups`,
+		});
+	}
+
+	return {
+		messages,
+	};
+};
