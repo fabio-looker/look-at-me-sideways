5b2894cc149cd09cc795254e4da322c56b01130f
jax <josephaxisa@gmail.com>
2018-11-07 14:58:35 -0500

    tests for settings.json using an fs mock

--- __tests__/tracking.test.js
+++ __tests__/tracking.test.js
@@ -74,19 +74,54 @@ describe('CLI', () => {
 			level: 'error',
 		}, {
 			rule: 'baz',
-			level: 'info' 
+			level: 'info',
 		}, {
 			rule: 'bat',
 			exempt: true,
 			level: 'warning',
-		}]
-		;
+		}];
 		tracker.track({messages, errors});
 		expect(write).toHaveBeenCalledWith(`v=1&av=0.0.0&tid=test&cid=834b0d78-07e7-4800-8493-db52fd650814&t=event&cd1=834b0d7807e798000493db52fd650814e534a8f742a1c5a58cbb7b42879696e0&cd2=&ec=Run&ea=End
 			v=1&av=0.0.0&tid=test&cid=834b0d78-07e7-4800-8493-db52fd650814&t=event&ec=Rule%20Result&ea=error&ev=2&cd3=foo&cd4=false&ni=1
 			v=1&av=0.0.0&tid=test&cid=834b0d78-07e7-4800-8493-db52fd650814&t=event&ec=Rule%20Result&ea=info&ev=1&cd3=baz&cd4=false&ni=1
 			v=1&av=0.0.0&tid=test&cid=834b0d78-07e7-4800-8493-db52fd650814&t=event&ec=Rule%20Result&ea=warning&ev=1&cd3=bat&cd4=false&ni=1`.replace(/\t+/g, ''));
 	});
-});
 
-// 2. test fs preferences saved preferences by passing that fs mock
+	// TODO: test the arguments with which fs.writeFileSync is called.
+	it('should save preferences when called with --reporting=save-yes', () => {
+		const fs = {
+			writeFileSync: () => {},
+			readFileSync: (path) => {
+				const fs = require('fs');
+				return fs.readFileSync(path);
+			},
+		};
+		const writeFileSync = jest.spyOn(fs, 'writeFileSync');
+		tracking({
+			cliArgs: {'reporting': 'save-yes'},
+			fs,
+		});
+
+		expect(writeFileSync).toHaveBeenCalled();
+	});
+
+
+	// TODO: make this test more specific
+	it('should not save preferences when called with --reporting=yes|no|save-no', () => {
+		const fs = {
+			writeFileSync: () => {},
+			readFileSync: (path) => {
+				const fs = require('fs');
+				return fs.readFileSync(path);
+			},
+		};
+
+		const writeFileSync = jest.spyOn(fs, 'writeFileSync');
+
+		tracking({
+			cliArgs: {'reporting': 'save-no'},
+			fs,
+		});
+		expect(writeFileSync).toHaveBeenCalled();
+	});
+});
