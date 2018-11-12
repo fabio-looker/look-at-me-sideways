6cac9f5a05ea0e37dfc77b833e865d5b87c7e6ff
fabio-looker <fabio@looker.com>
2018-11-12 14:36:21 -0500

    Fixed e1 when sql & sql_on are missing

--- rules/e1.js
+++ rules/e1.js
@@ -15,7 +15,7 @@ module.exports = function(
 			for (let join of joins) {
 				let location = `model:${model._model}/explore:${explore._explore}/join:${join._join}`;
 				let exempt = getExemption(join, rule) || getExemption(explore, rule) || getExemption(model, rule);
-				let sql = join.sql || join.sql_on;
+				let sql = join.sql || join.sql_on || '';
 				let sqlWithLkmlRemoved = sql.replace(/\${[\s\S]*?}|{{[\s\S]*?}}|{%\s*if[\s\S]*?endif\s*%}|{%[\s\S]*?%}/g, '' );
 				let references = (sqlWithLkmlRemoved.match(/[a-zA-Z0-9._]+\.[a-zA-Z0-9 ._]+/g)||[])
 					.filter((ref) => !( // Two-part references to allow
